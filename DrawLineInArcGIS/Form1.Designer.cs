6e21b875e20ef7d1514d3ba4c685933027e49dee
Coder BB <2512275982@qq.com>
2017-11-21 18:04:21 +0800

    第一次提交

---
+++ DrawLineInArcGIS/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace DrawLineInArcGIS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
+            this.axToolbarControl1 = new ESRI.ArcGIS.Controls.AxToolbarControl();
+            this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axMapControl1
+            // 
+            this.axMapControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axMapControl1.Location = new System.Drawing.Point(0, 0);
+            this.axMapControl1.Name = "axMapControl1";
+            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
+            this.axMapControl1.Size = new System.Drawing.Size(1360, 688);
+            this.axMapControl1.TabIndex = 0;
+            // 
+            // axToolbarControl1
+            // 
+            this.axToolbarControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.axToolbarControl1.Location = new System.Drawing.Point(0, 0);
+            this.axToolbarControl1.Name = "axToolbarControl1";
+            this.axToolbarControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axToolbarControl1.OcxState")));
+            this.axToolbarControl1.Size = new System.Drawing.Size(1360, 28);
+            this.axToolbarControl1.TabIndex = 1;
+            // 
+            // axLicenseControl1
+            // 
+            this.axLicenseControl1.Enabled = true;
+            this.axLicenseControl1.Location = new System.Drawing.Point(1072, 181);
+            this.axLicenseControl1.Name = "axLicenseControl1";
+            this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
+            this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1190, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "三角格网";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1190, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "清除图形";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1190, 248);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "矩形格网";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1360, 688);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axLicenseControl1);
+            this.Controls.Add(this.axToolbarControl1);
+            this.Controls.Add(this.axMapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+        private ESRI.ArcGIS.Controls.AxToolbarControl axToolbarControl1;
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ DrawLineInArcGIS/Form1.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Display;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.GlobeCore;
+using ESRI.ArcGIS.SystemUI;
+using Hykj.MeteData;
+using Hykj.PubMethods;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.ADF.Connection.Local;
+using ESRI.ArcGIS.ADF;
+
+namespace DrawLineInArcGIS
+{
+    public partial class Form1 : Form
+    {
+        private List<PointInfo> tempListPntInfo = new List<PointInfo>();
+        private Delauney delaumey;
+        private GridClass gridClass;
+
+        public Form1()
+        {
+            InitializeComponent();
+            IAoInitialize lisence = new AoInitializeClass();
+            lisence.Initialize(esriLicenseProductCode.esriLicenseProductCodeEngineGeoDB);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.axMapControl1.LoadMxFile(Application.StartupPath + "\\map.mxd");
+        }
+
+        private void GetPntShape(string type)
+        {
+            DataTable valueTable = null;
+
+            if (type == "Triangle")
+            {
+                tempListPntInfo.Clear();
+                string url = "http://218.28.7.251:10525/hnqxjson/QxSqlInter/findDataSetOnDataType.hd?dataType=1-3-2&cityCode=HN";
+                string logMessage;
+                valueTable = MeteData.GetValueFromJson(url, out logMessage);
+
+                if (!string.IsNullOrEmpty(logMessage))
+                {
+                    MessageBox.Show(logMessage);
+                    return;
+                }
+            }
+            else if (type == "Rectangle")
+            {
+                string filePath = @"E:\Book1.xlsx";
+                valueTable = MeteData.GetValueFromExcel(filePath);
+            }
+
+            if (valueTable == null)
+            {
+                MessageBox.Show("数据读取失败！");
+                return;
+            }
+
+            DataView dataView = valueTable.DefaultView;
+            dataView.Sort = "longitude";
+            DataTable sortTable = dataView.ToTable();
+
+            try
+            {
+                for (int i = 0; i < sortTable.Rows.Count;i++ )
+                {
+                    DataRow dr = sortTable.Rows[i];
+                    if (dr["longitude"] == DBNull.Value || dr["longitude"] == null)
+                        continue;
+                    if (dr["latitude"] == DBNull.Value || dr["latitude"] == null)
+                        continue;
+                    if (dr["eValue"] == DBNull.Value || dr["eValue"] == null)
+                        continue;
+                    string stationId = dr["stationID"].ToString();
+                    System.Text.RegularExpressions.Regex reg = new System.Text.RegularExpressions.Regex(@"^5.*");
+                    if (!reg.IsMatch(stationId))
+                        continue;
+                    double x = double.Parse(dr["longitude"].ToString());
+                    double y = double.Parse(dr["latitude"].ToString());
+                    double eValue = double.Parse(dr["eValue"].ToString());
+                    if (x < 110.35 || x > 116.65)
+                        continue;
+                    if (y < 31.383 || y > 36.37)
+                        continue;
+                    if (eValue < 5)
+                        continue;
+
+                    //double height = double.Parse(dr["height"].ToString());
+                    //if (height < 60)
+                    //    continue;
+
+                    for (int rowIndexAdd = 1; i + rowIndexAdd < sortTable.Rows.Count; rowIndexAdd++)
+                    {
+                        DataRow compareRow = sortTable.Rows[i + rowIndexAdd];
+                        if (compareRow["longitude"] == DBNull.Value || compareRow["longitude"] == null)
+                            continue;
+                        if (compareRow["latitude"] == DBNull.Value || compareRow["latitude"] == null)
+                            continue;
+
+                        double cX = double.Parse(compareRow["longitude"].ToString());
+                        double cY = double.Parse(compareRow["latitude"].ToString());
+                        if (x == cX && y == cY)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            i = i + rowIndexAdd - 1;
+                            break;
+                        }
+                    }
+
+                    PointInfo pntInfo = new PointInfo(x, y, eValue);
+                    tempListPntInfo.Add(pntInfo);
+                }
+                DateTime timeStart = DateTime.Now;
+
+                //gridClass = new GridClass(tempListPntInfo);
+                //gridClass.GetSuperRectangle();
+                //gridClass.GetIsobands();
+                delaumey = new Delauney(tempListPntInfo);
+                delaumey.GetDelauney();
+                delaumey.Isoline(new double[] { 9,10,11,12,14 });
+
+
+                TimeSpan timeSpan = DateTime.Now - timeStart;
+                EditMap();
+                //MessageBox.Show(delaumey.ListTriangles.Count.ToString() + "     " + timeSpan.TotalSeconds.ToString());
+                MessageBox.Show(timeSpan.TotalSeconds.ToString());
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void EditMap()
+        {
+            IMap map = this.axMapControl1.Map;
+            ILayer polyLayer = map.get_Layer(2);
+            IFeatureLayer featLayer = polyLayer as IFeatureLayer;
+            IFeatureClass featClass = featLayer.FeatureClass;
+
+            ILayer lineLayer = map.get_Layer(1);
+            IFeatureLayer featLineLayer = lineLayer as IFeatureLayer;
+            IFeatureClass featLineClass = featLineLayer.FeatureClass;
+
+            ILayer pntLayer = map.get_Layer(0);
+            IFeatureLayer pntFeatLayer = pntLayer as IFeatureLayer;
+            IFeatureClass pntClass = pntFeatLayer.FeatureClass;
+
+            IFeatureDataset featDataset = featClass.FeatureDataset;
+            IWorkspace workspace = featDataset.Workspace;
+            IWorkspaceEdit workspaceEdit = workspace as IWorkspaceEdit;
+            workspaceEdit.StartEditing(false);
+            workspaceEdit.StartEditOperation();
+
+            //using (ComReleaser comReleaser = new ComReleaser())
+            //{
+            //    int index = pntClass.FindField("Value");
+            //    IFeatureBuffer featBuffer = pntClass.CreateFeatureBuffer();
+            //    comReleaser.ManageLifetime(featBuffer);
+
+            //    IFeatureCursor insertCursor = pntClass.Insert(true);
+            //    comReleaser.ManageLifetime(insertCursor);
+            //    for (int i = 0; i < gridClass.PntGrid.Length; i++)
+            //    {
+            //        for (int j = 0; j < gridClass.PntGrid[i].Length; j++)
+            //        {
+            //            PointInfo pntInfo = gridClass.PntGrid[i][j];
+            //            if (pntInfo == null)
+            //                continue;
+            //            IPoint pnt = new PointClass();
+            //            pnt.X = pntInfo.X;
+            //            pnt.Y = pntInfo.Y;
+
+            //            featBuffer.Shape = pnt;
+            //            featBuffer.set_Value(index, pntInfo.EValue);
+            //            insertCursor.InsertFeature(featBuffer);
+            //        }
+            //    }
+            //    insertCursor.Flush();
+            //}
+
+            //using (ComReleaser comReleaser = new ComReleaser())
+            //{
+            //    int index = featClass.FindField("VALUE");
+
+            //    IFeatureBuffer featBuffer = featClass.CreateFeatureBuffer();
+            //    comReleaser.ManageLifetime(featBuffer);
+
+            //    IFeatureCursor insertCursor = featClass.Insert(true);
+            //    comReleaser.ManageLifetime(insertCursor);
+            //    foreach (PolygonInfo poly in gridClass.ListPolys)
+            //    {
+            //        IPointCollection pntColl = new PolygonClass();
+            //        for (int i = 0; i < poly.ListPolyVertrix.Count; i++)
+            //        {
+            //            IPoint pnt = new PointClass();
+            //            pnt.X = poly.ListPolyVertrix[i].X;
+            //            pnt.Y = poly.ListPolyVertrix[i].Y;
+            //            pntColl.AddPoint(pnt);
+            //        }
+            //        IPoint pntStart = new PointClass();
+            //        pntStart.X = poly.ListPolyVertrix[0].X;
+            //        pntStart.Y = poly.ListPolyVertrix[0].Y;
+            //        pntColl.AddPoint(pntStart);
+
+            //        IPolygon polygon = pntColl as IPolygon;
+
+            //        featBuffer.Shape = polygon;
+            //        featBuffer.set_Value(index, poly.ColorRange);
+            //        insertCursor.InsertFeature(featBuffer);
+            //    }
+            //    insertCursor.Flush();
+            //}
+
+            using (ComReleaser comReleaser = new ComReleaser())
+            {
+                int index = pntClass.FindField("Value");
+                IFeatureBuffer featBuffer = pntClass.CreateFeatureBuffer();
+                comReleaser.ManageLifetime(featBuffer);
+
+                IFeatureCursor insertCursor = pntClass.Insert(true);
+                comReleaser.ManageLifetime(insertCursor);
+                foreach (PointInfo pntInfo in tempListPntInfo)
+                {
+                    IPoint pnt = new PointClass();
+                    pnt.X = pntInfo.X;
+                    pnt.Y = pntInfo.Y;
+
+                    featBuffer.Shape = pnt;
+                    featBuffer.set_Value(index, pntInfo.EValue);
+                    insertCursor.InsertFeature(featBuffer);
+                }
+                insertCursor.Flush();
+            }
+
+            using (ComReleaser comReleaser = new ComReleaser())
+            {
+                int index = featLineClass.FindField("Value");
+                IFeatureBuffer featBuffer = featLineClass.CreateFeatureBuffer();
+                comReleaser.ManageLifetime(featBuffer);
+
+                IFeatureCursor insertCursor = featLineClass.Insert(true);
+                comReleaser.ManageLifetime(insertCursor);
+                foreach (IsolineClass isoLine in delaumey.ListIsoLines)
+                {
+                    IPointCollection pntColl = new PolylineClass();
+                    for (int i = 0; i < isoLine.pntList.Length; i++)
+                    {
+                        IPoint pnt = new PointClass();
+                        pnt.X = isoLine.pntList[i].X;
+                        pnt.Y = isoLine.pntList[i].Y;
+
+                        pntColl.AddPoint(pnt);
+                    }
+
+                    IPolyline line = pntColl as IPolyline;
+
+                    featBuffer.Shape = line;
+                    featBuffer.set_Value(index, isoLine.IsolineValue);
+                    insertCursor.InsertFeature(featBuffer);
+                }
+                insertCursor.Flush();
+            }
+
+            using (ComReleaser comReleaser = new ComReleaser())
+            {
+                IFeatureBuffer featBuffer = featClass.CreateFeatureBuffer();
+                comReleaser.ManageLifetime(featBuffer);
+
+                IFeatureCursor insertCursor = featClass.Insert(true);
+                comReleaser.ManageLifetime(insertCursor);
+                foreach (TriangleInfo triangle in delaumey.ListTriangles)
+                {
+                    IPointCollection pntColl = new PolygonClass();
+                    IPoint pntA = new PointClass();
+                    pntA.X = triangle.VertexA.X;
+                    pntA.Y = triangle.VertexA.Y;
+                    pntColl.AddPoint(pntA);
+
+                    IPoint pntB = new PointClass();
+                    pntB.X = triangle.VertexB.X;
+                    pntB.Y = triangle.VertexB.Y;
+                    pntColl.AddPoint(pntB);
+
+                    IPoint pntC = new PointClass();
+                    pntC.X = triangle.VertexC.X;
+                    pntC.Y = triangle.VertexC.Y;
+                    pntColl.AddPoint(pntC);
+                    pntColl.AddPoint(pntA);
+
+                    IPolygon polygon = pntColl as IPolygon;
+
+                    featBuffer.Shape = polygon;
+                    insertCursor.InsertFeature(featBuffer);
+                }
+                insertCursor.Flush();
+            }
+
+            workspaceEdit.StopEditOperation();
+            workspaceEdit.StopEditing(true);
+        }
+
+        private void Delete2()
+        {
+            IMap map = this.axMapControl1.Map;
+            ILayer pntLayer = map.get_Layer(0);
+            IFeatureLayer featLayer = pntLayer as IFeatureLayer;
+            IFeatureClass featClass = featLayer.FeatureClass;
+
+            ILayer lineLayer = map.get_Layer(1);
+            IFeatureLayer featLineLayer = lineLayer as IFeatureLayer;
+            IFeatureClass featLineClass = featLineLayer.FeatureClass;
+
+            ILayer polygonLayer = map.get_Layer(2);
+            IFeatureClass polygonClass = (polygonLayer as IFeatureLayer).FeatureClass;
+
+            IFeatureDataset featDataset = featClass.FeatureDataset;
+            IWorkspace workspace = featDataset.Workspace;
+            IWorkspaceEdit workspaceEdit = workspace as IWorkspaceEdit;
+            workspaceEdit.StartEditing(false);
+            workspaceEdit.StartEditOperation();
+
+            IQueryFilter pQueryFilter = new QueryFilterClass();
+            pQueryFilter.WhereClause = "";
+
+            ITable table = (ITable)featClass;
+            table.DeleteSearchedRows(pQueryFilter);
+
+            ITable lineTable = (ITable)featLineClass;
+            lineTable.DeleteSearchedRows(pQueryFilter);
+
+            ITable polygonTable = (ITable)polygonClass;
+            polygonTable.DeleteSearchedRows(pQueryFilter);
+
+
+            //IFeatureCursor pFeatureCursor = featClass.Update(pQueryFilter, false);
+            //IFeature pFeature = pFeatureCursor.NextFeature();
+            //while (pFeature != null)
+            //{
+            //    pFeatureCursor.DeleteFeature();
+            //    pFeature = pFeatureCursor.NextFeature();
+            //}
+            //System.Runtime.InteropServices.Marshal.ReleaseComObject(pQueryFilter);
+
+            workspaceEdit.StopEditOperation();
+            workspaceEdit.StopEditing(true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetPntShape("Triangle");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Delete2();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
